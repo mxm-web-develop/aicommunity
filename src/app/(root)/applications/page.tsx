6219ed946f794b45d45b07d9be080ed509fddbe7
mxmweb <mxm.web.develop@gmail.com>
2025-02-20 19:39:49 +0800

    找回丢失的头部

--- src/app/(root)/applications/page.tsx
+++ src/app/(root)/applications/page.tsx
@@ -31,6 +31,27 @@ export default async function ApplicationsPage() {
     const applications2 = Array.isArray(AIapplications2) ? AIapplications2 : [];
 
     return (
+      <div className="w-full">
+      <div className="select-none h-[248px] relative w-full mb-10">
+        <div className="absolute top-12 left-0 right-0 z-10">
+          <div className="container">
+            <div className="text-[40px]">
+              <b>产品与服务</b>
+            </div>
+            <div className="text-[#333] mb-4">
+              突破性AI技术，开启无限创新机遇
+            </div>
+            <SearchBar />
+          </div>
+        </div>
+        <Image
+          src={applicationsBanner}
+          alt=""
+          fill
+          className="object-cover"
+          priority
+        />
+      </div>      
       <div className="container mx-auto py-8">
         <div className="application-displayer py-5">
           <div className="application-displayer-title text-2xl font-bold text-center">
@@ -53,6 +74,7 @@ export default async function ApplicationsPage() {
             ))}
           </div>
         </div>
+      </div>
       </div>
     );
   } catch (error) {
